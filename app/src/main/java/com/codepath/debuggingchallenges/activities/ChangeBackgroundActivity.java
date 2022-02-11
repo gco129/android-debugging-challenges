f2e2aa05b6d1d5ee5ac7b92165229944566cb161
gco129 <gian.ordonez@gmail.com>
2022-02-11 10:38:04 -0500

    Part 1

--- app/src/main/java/com/codepath/debuggingchallenges/activities/ChangeBackgroundActivity.java
+++ app/src/main/java/com/codepath/debuggingchallenges/activities/ChangeBackgroundActivity.java
@@ -15,6 +15,7 @@ public class ChangeBackgroundActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_background);
+        findViewById(android.R.id.content).setBackgroundColor(oldColor);
     }
 
     public void onGoClick(View view) {
